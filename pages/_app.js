a76d3da92219e4f8de5a356bd57a1b833c1707a6
codewithtusharvaswani <codewithtusharvaswani@gmail.com>
2022-07-10 10:06:49 +0530

    Tawk.to integrated

--- pages/_app.js
+++ pages/_app.js
@@ -50,6 +50,19 @@ function MyApp({ Component, pageProps }) {
                 strategy="lazyOnload"
                 src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
             />
+            <Script id="tawk">
+                {`
+                    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+                    (function(){
+                    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                    s1.async=true;
+                    s1.src='https://embed.tawk.to/62ca51327b967b117998d409/1g7j4qbfo';
+                    s1.charset='UTF-8';
+                    s1.setAttribute('crossorigin','*');
+                    s0.parentNode.insertBefore(s1,s0);
+                    })();
+            `}
+            </Script>
             <Script strategy="lazyOnload" id="start">
                 {`
                     window.dataLayer = window.dataLayer || [];
